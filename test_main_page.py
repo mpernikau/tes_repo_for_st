e16f0f5cbd66aade0e1aaefcfae0407814031e86
Maksim Pernikau <mpernikau@gmail.com>
2022-04-27 18:50:33 +0300

    Added assert + NoSuchElementException assert

--- test_main_page.py
+++ test_main_page.py
@@ -1,15 +1,19 @@
 import pytest
 from selenium.webdriver.common.by import By
+from pages.main_page import MainPage
 
 
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
-def test_add_to_basket_button(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    browser.get(link)
-    #time.sleep(15)
-
-    button_exists = browser.find_elements(By.ID, 'add_to_basket_form')
-    assert button_exists, 'no button'
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
 
 if __name__ == "__main__":
