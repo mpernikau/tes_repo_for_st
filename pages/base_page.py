925258473d63603142bbc741bf6370de3568fb81
Maksim Pernikau <mpernikau@gmail.com>
2022-04-28 10:26:34 +0300

    1. Changes in base_page and main page 2. adding locators file and login_page file 3. deleting conftest from wrong directory

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,17 +1,20 @@
 from selenium.common.exceptions import NoSuchElementException
 
+
 class BasePage():
-    def __init__(self, browser, url, timeout=10): #Конструктор — метод, который вызывается, когда мы создаем объект. Конструктор объявляется ключевым словом __init__
+    def __init__(self, browser, url, timeout=10):  #Конструктор — метод, который вызывается, когда мы создаем объект. Конструктор объявляется ключевым словом __init__
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
 
+
     def open(self):
         self.browser.get(self.url)
 
-    def is_element_present(self, how, what):
+
+    def is_element_present(self, CSS_SELECTOR, login_link):
         try:
-            self.browser.find_element(how, what)
+            self.browser.find_element(CSS_SELECTOR, login_link)
         except (NoSuchElementException):
             return False
         return True
