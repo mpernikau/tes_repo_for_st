b656bf3392aba0dd5049ec339bea899f625adbd4
Maksim Pernikau <mpernikau@gmail.com>
2022-05-05 16:44:59 +0300

    Changed tests a lot, reworked locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -9,5 +9,8 @@ class LoginPageLocators():
     REGISTRATION_FORM = (By.ID, 'register_form')
 
 class ProductPageLocators():
-    BASKET_BUTTON = (By.CLASS_NAME, 'btn.btn-lg.btn-primary.btn-add-to-basket')
-    PROMO_LINK = (By.CSS_SELECTOR, "#promo_link")
+    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, "div.alertinner ")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
--- pages/test_product_page.py
+++ pages/test_product_page.py
@@ -1,24 +1,11 @@
-import pytest
-from selenium.webdriver.common.by import By
-from pages.main_page import MainPage
 from pages.product_page import ProductPage
-import time
 
-def test_guest_should_be_on_promo_link(browser):
+
+def test_guest_can_add_product_to_cart(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
     page.open()
-    page.should_be_promo_url()
-    time.sleep(5)
-
-def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_add_to_basket()
-    page.button_is_clickable()
+    page.press_button_add_to_basket()
     page.solve_quiz_and_get_code()
-    time.sleep(5)
-
-if __name__ == "__main__":
-    pytest.main()
+    page.should_be_message_about_adding()
+    page.should_be_message_basket_total()
