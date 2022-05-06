c9fb93aa7c763b240b91752bd9b9f7d397c60868
Maksim Pernikau <mpernikau@gmail.com>
2022-05-06 16:07:58 +0300

    Added new parametrize, changed locator to be more definite

--- pages/locators.py
+++ pages/locators.py
@@ -10,7 +10,7 @@ class LoginPageLocators():
 
 class ProductPageLocators():
     BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
-    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, "div.alertinner ")
-    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, "div.alertinner strong")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.col-sm-6.product_main h1")
     MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
